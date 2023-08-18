f981ccc12f0a198f6519568fe7eda27e4e5aaebc
mikej27 <mikej9727@yahoo.com>
2023-08-18 19:08:18 -0400

    Added components to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+function BookList (){
+return( 
+    <>
+      <Book/>
+    </>
+  )
+}
+
+const Book = () => {
+return(
+  <section>
+    <BookImage />
+    <BookTitle />
+    <BookAuthor />
+  </section>
+)
+}
+
+const BookImage = () => <img src="#"></img>
+const BookTitle = () => <h2>Book Title</h2>
+const BookAuthor = () => <h4>Book Author</h4>
 
 
-const root = ReactDom.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(<BookList/>)
